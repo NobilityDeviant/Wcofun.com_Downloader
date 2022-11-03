532baf5a32f3bfedc65d325e033dab591f14be65
Nobility <nobilitydev@protonmail.com>
2022-11-03 06:04:44 -0600

    Updated version inside UpdateManager.kt Oops!

--- src/main/java/com/nobility/downloader/updates/UpdateManager.kt
+++ src/main/java/com/nobility/downloader/updates/UpdateManager.kt
@@ -146,7 +146,7 @@ class UpdateManager(private val model: Model) {
     }
 
     companion object {
-        const val CURRENT_VERSION = "1.4.9"
+        const val CURRENT_VERSION = "1.5"
         const val RELEASES_LINK = "https://github.com/NobilityDeviant/Wcofun.com_Downloader/releases"
     }
 }
